62a1793381c4cdcdccb438ef8abd86cb24f21161
vasuans <srinivasu.addanki@yahoo.com>
2020-09-23 12:13:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "\
+echo “\
 [general]\n\
-email = \"srinivasu.addanki@yahoo.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+email = \”srinivasu.addanki@yahoo.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
