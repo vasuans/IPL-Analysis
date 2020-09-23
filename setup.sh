92f8c2a07ed80ab5aca67de01b21fbad04c2dd25
vasuans <srinivasu.addanki@yahoo.com>
2020-09-23 12:47:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
